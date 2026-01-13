53d1ee3b873208363bb986bdcb7240be8506bc89
Fabio Marsiaj <fabiobackend@gmail.com>
2026-01-13 16:40:00 -0300

    fix on main

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: true,
-      forbidNonWhitelisted: true,
-    }),
-  );
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
